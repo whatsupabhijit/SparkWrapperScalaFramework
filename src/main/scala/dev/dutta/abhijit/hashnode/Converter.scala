9866be07c4df9acee3bfd810799275a75025592c
whatsupabhijit <tech.abhijit.dutta@gmail.com>
2023-06-05 16:32:24 +0530

    Added datatype converters

---
+++ src/main/scala/dev/dutta/abhijit/hashnode/Converter.scala
@@ -0,0 +1,47 @@
+package dev.dutta.abhijit.hashnode
+
+import org.apache.spark.sql.execution.streaming.FileStreamSource.Timestamp
+import org.apache.spark.sql.types.{BooleanType, DataType, DoubleType, FloatType, IntegerType, LongType, StringType, TimestampType}
+
+import java.io.Serializable
+
+/**
+ * This is mainly used to limit the output type of Atom.
+ * This also helps in mapping scala Type to corresponding Spark data types
+ * @tparam O Output Type of the Atom
+ * */
+sealed trait Converter[O] extends Serializable {
+  def dataType: DataType
+}
+
+object Converter {
+
+  implicit val intConverter: Converter[Int] = new Converter[Int] {
+    override def dataType: DataType = IntegerType
+  }
+
+  implicit val longConverter: Converter[Long] = new Converter[Long] {
+    override def dataType: DataType = LongType
+  }
+
+  implicit val floatConverter: Converter[Float] = new Converter[Float] {
+    override def dataType: DataType = FloatType
+  }
+
+  implicit val doubleConverter: Converter[Double] = new Converter[Double] {
+    override def dataType: DataType = DoubleType
+  }
+
+  implicit val booleanConverter: Converter[Boolean] = new Converter[Boolean] {
+    override def dataType: DataType = BooleanType
+  }
+
+  implicit val timestampConverter: Converter[Timestamp] = new Converter[Timestamp] {
+    override def dataType: DataType = TimestampType
+  }
+
+  implicit val stringConverter: Converter[String] = new Converter[String] {
+    override def dataType: DataType = StringType
+  }
+
+}
