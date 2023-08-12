9e91196cef28e242dfa5ca237a440020e90afb26
whatsupabhijit <tech.abhijit.dutta@gmail.com>
2023-08-12 13:58:35 +0530

    completed regulations package components

--- src/main/scala/dev/dutta/abhijit/hashnode/regulations/Permissions.scala
+++ src/main/scala/dev/dutta/abhijit/hashnode/regulations/Permissions.scala
@@ -2,6 +2,7 @@ package dev.dutta.abhijit.hashnode.regulations
 
 import dev.dutta.abhijit.hashnode.constants.IntConstants._
 import dev.dutta.abhijit.hashnode.constants.StringConstants._
+import dev.dutta.abhijit.hashnode.regulations.AtomicRule.RulesDerivation
 
 case class Permissions(
                       isPermission1: Boolean = false,
@@ -13,10 +14,10 @@ object Permissions {
 
   def fromRules(atomicRule: AtomicRule): Permissions =
     Permissions(
-      isPermission1 = atomicRule.getPermission[Boolean](AtomicRule.PERMISSION_1),
-      isPermission2 = atomicRule.getPermission[Boolean](AtomicRule.PERMISSION_2),
-      permission3 = atomicRule.getPermission[String](AtomicRule.PERMISSION_3),
-      permission4 = atomicRule.getPermission[Int](AtomicRule.PERMISSION_4)
+//      isPermission1 = atomicRule.ruleGroup.getPermission[Boolean](AtomicRule.PERMISSION_1),
+//      isPermission2 = atomicRule.ruleGroup.getPermission[Boolean](AtomicRule.PERMISSION_2),
+      permission3 = atomicRule.ruleGroup.getPermission[String](AtomicRule.PERMISSION_3),
+      permission4 = atomicRule.ruleGroup.getPermission[Int](AtomicRule.PERMISSION_4)
     )
 
 }
